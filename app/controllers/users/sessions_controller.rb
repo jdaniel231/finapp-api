7e4a1b9477277ce9d45187a95bcbf2672d030fe2
jdaniel231 <jdaniel231@gmail.com>
2025-05-25 23:25:11 -0300

    devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: { code: 200, message: "Logged in successfully." },
+      status: { code: 200, message: "Logged in sucesso." },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
   end
