87c2cab77ecc401da7f97d948991acead8595feb
jdaniel231 <jdaniel231@gmail.com>
2025-05-31 22:26:41 -0300

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,11 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
+    token = encode_token({ user_id: resource.id })
+
     render json: {
       status: { code: 200, message: "Logged in successfully." },
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      data: UserSerializer.new(resource).serializable_hash[:data][:attributes].merge({ token: token })
     }, status: :ok
   end
 
@@ -22,5 +24,9 @@ class Users::SessionsController < Devise::SessionsController
         message: "Couldn't find an active session."
       }, status: :unauthorized
     end
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secret_key_base)
   end
 end
