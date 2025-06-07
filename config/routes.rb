5ef7fbcfd6f7061e59832919e6373d9ee0469964
jdaniel231 <jdaniel231@gmail.com>
2025-06-06 22:29:56 -0300

    tipo de pagamento

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :clients
       resources :services
+      resources :payment_types
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_06_024136) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_06_030216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_06_024136) do
   create_table "jwt_denylists", force: :cascade do |t|
     t.string "jti"
     t.datetime "exp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payment_types", force: :cascade do |t|
+    t.string "name"
+    t.decimal "taxa"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
